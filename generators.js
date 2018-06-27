4d513a11967e2f4af564e750306ed6f417eb8d15
Bala Krishna Ragala <baluragala@gmail.com>
2018-06-27 20:05:51 +0530

    day3-es6

---
+++ generators.js
@@ -0,0 +1,52 @@
+function step1() {
+  return new Promise((resolve, reject) => {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    setTimeout(function() {
+      console.log(4);
+      console.log(5);
+      resolve();
+    }, 1000);
+  });
+}
+
+function step2() {
+  console.log(6);
+}
+
+function step3() {
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      console.log(7);
+      console.log(8);
+      resolve();
+    }, 1000);
+  });
+}
+
+function step4() {
+  console.log(9);
+  console.log(10);
+}
+
+// function* doWork() {
+//   console.log(1);
+//   console.log(2);
+//   yield 1;
+//   console.log(3);
+//   console.log(4);
+//   yield 2;
+//   console.log(5);
+//   console.log(6);
+//   return 344;
+// }
+
+function* doWork() {
+  yield step1();
+  step2();
+  yield step3();
+  step4();
+}
+
+doWork();
---
+++ lib.js
@@ -0,0 +1,7 @@
+export function first(array) {
+  return array[0];
+}
+
+export function last(array) {
+  return array[array.length - 1];
+}
