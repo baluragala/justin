671ef1624ea4693f31cf5fe7b1ab2343977fc56a
Bala Krishna Ragala <baluragala@gmail.com>
2018-06-28 19:34:01 +0530

    day 3 react intro

---
+++ estore/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import "./App.css";
+import Footer from "./Footer";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ estore/src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer>
+      <p>EStore, all rights reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
